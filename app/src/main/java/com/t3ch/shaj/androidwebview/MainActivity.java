1971e19449335c1622270e851bea0d0dbd6b9ef0
SHAJ <shakilahmedshaj@gmail.com>
2018-08-25 15:48:57 +0600

    Initial commit

---
+++ app/src/main/java/com/t3ch/shaj/androidwebview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.t3ch.shaj.androidwebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.WebView_id);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("www.shakilahmedshaj.com/");
+
+    }
+}
